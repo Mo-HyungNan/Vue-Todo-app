617ebd21c97f80ef619303a826f51679b68b9859
Mo-HyungNan <hyhy321@hanmail.net>
2020-11-20 05:00:51 +0900

    setting complete

--- webpack.config.js
+++ webpack.config.js
@@ -7,78 +7,78 @@ const merge = require('webpack-merge')
 require('@babel/polyfill')
 
 module.exports = (env, opts) => {
-    const config = {
-        resolve: {
-            extensions: ['.js', '.vue']
+  const config = {
+    resolve: {
+      extensions: ['.js', '.vue']
+    },
+    entry: {
+      app: [
+        '@babel/polyfill',
+        path.join(__dirname, 'main.js')
+      ]
+    },
+    output: {
+      filename: '[name]'.js,
+      path: path.join(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: 'vue-loader'
         },
-        entry: {
-            app: [
-                '@babel/polyfill',
-                path.join(__dirname, 'main.js')
-            ]
+        {
+          test: /\.?js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
         },
-        output: {
-            filename: '[name]'.js,
-            path: path.join(__dirname, 'dist')
+        {
+          test: /\.css$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'postcss-loader'
+          ]
         },
-        module: {
-            rules: [
-                {
-                    test: /\.vue$/,
-                    use: 'vue-loader'
-                },
-                {
-                    test: /\.?js$/,
-                    exclude: /node_modules/,
-                    use: 'babel-loader'
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        'vue-style-loader',
-                        'css-loader',
-                        'postcss-loader',
-                    ]
-                },
-                {
-                    test: /\.scss$/,
-                    use: [
-                        'vue-style-loader',
-                        'css-loader',
-                        'postcss-loader',
-                        'sass-loader'
-                    ]
-                }
-            ]
-        },
-        plugins: [
-            new VueLoaderPlugin(),
-            new HtmlWebpackPlugin({
-                template: path.join(__dirname, 'index.html')
-            }),
-            new CopyPlugin([
-                {
-                   from: 'assets/',
-                   to: ''
-                }
-            ])
-        ]
-    }
+        {
+          test: /\.scss$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'postcss-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'index.html')
+      }),
+      new CopyPlugin([
+        {
+          from: 'assets/',
+          to: ''
+        }
+      ])
+    ]
+  }
 
-    if (opts.mode === 'development') {
-        return merge(config, {
-            devtool: 'eval',
-            devServer: {
-                open: false,
-                hot: true
-            }
-        })
-    } else {
-        return merge(config, {
-        devtool: 'cheap-module-source-map',
-        plugins: [
-            new CleanWebpackPlugin()
-        ]
-        })
-    }
+  if (opts.mode === 'development') {
+    return merge(config, {
+      devtool: 'eval',
+      devServer: {
+        open: false,
+        hot: true
+      }
+    })
+  } else {
+    return merge(config, {
+      devtool: 'cheap-module-source-map',
+      plugins: [
+        new CleanWebpackPlugin()
+      ]
+    })
+  }
 }
